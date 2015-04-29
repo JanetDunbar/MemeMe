18ae5a0a26f4c195720e3298f1e878cdaeb0e352
jmd <jmd@amberlight.com>
2015-04-29 11:12:32 -0500

    Initial Commit

---
+++ MemeMePrelim3Tests/MemeMePrelim3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MemeMePrelim3Tests.swift
+//  MemeMePrelim3Tests
+//
+//  Created by Dr. Janet M. Dunbar on 4/29/15.
+//  Copyright (c) 2015 Dr. Janet M. Dunbar. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MemeMePrelim3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
