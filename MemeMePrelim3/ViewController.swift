05b73ffb46ada991e042b4889a25aaf3e0cc1322
jmd <jmd@amberlight.com>
2015-04-29 18:20:40 -0500

    Copied in code for viewController.swift code from MemeMe Prelim 2.  Connected IBoutlet and Inaction.  Changed contentMode to Scale Aspect Fit.  Now image picker works and displays its image correctly.  Also toolbar and toolbar items display correctly!

--- MemeMePrelim3/ViewController.swift
+++ MemeMePrelim3/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func pickAnImage(sender: AnyObject) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imagePickerView.image = image
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
 }
 
