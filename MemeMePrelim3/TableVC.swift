ca782a636d587bf49f19212658d17c046feebb6a
jmd <jmd@amberlight.com>
2015-05-17 17:05:37 -0500

    Added ShowMemeDetail segue between TableVC and detail.  Tested camera again - getting error to fix ? image picker not being dismissed correctly?

--- MemeMePrelim3/TableVC.swift
+++ MemeMePrelim3/TableVC.swift
@@ -34,12 +34,6 @@ class TableVC: UITableViewController, UITableViewDataSource, UITableViewDelegate
 
     // MARK: - Table view data source
 
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        // #warning Potentially incomplete method implementation.
-//        // Return the number of sections.
-//        return 0
-//    }
-
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
