59cafd1dcbc8c1d3a9c4a2e0a1c0fb6e2e177cb7
jmd <jmd@amberlight.com>
2015-05-19 16:40:45 -0500

    Took away add button functionality because of too many bugs.  App works mostly.  Need to fix camera error and possibly try implementing edit button.  Also could beautify graphics.

--- MemeMePrelim3/ViewController.swift
+++ MemeMePrelim3/ViewController.swift
@@ -80,6 +80,7 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
         // Subscribe to keyboard notifications to allow the view to raise when necessary
         self.subscribeToKeyboardNotifications()
         self.subscribeToKeyboardHideNotifications()
+        //self.tabBarController?.tabBar.hidden = true
         
         
     }
