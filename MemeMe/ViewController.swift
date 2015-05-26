05babf6b6ade9a6ce92eff5266c22c1ace757dfd
jmd <jmd@amberlight.com>
2015-05-26 13:03:07 -0500

    Fixed 2 keyboard bugs per code review.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -94,18 +94,23 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
     
     func keyboardWillShow(notification: NSNotification) {
         if bottomTextField.isFirstResponder() {
-            self.view.frame.origin.y -= getKeyboardHeight(notification)
+            // Original code below
+            //self.view.frame.origin.y -= getKeyboardHeight(notification)
+            self.view.frame.origin.y = -(getKeyboardHeight(notification))
+            
         }
     }
     
     func keyboardWillHide(notification: NSNotification) {
       
         if bottomTextField.isFirstResponder() {
+            // Original code below
+            //self.view.frame.origin.y += getKeyboardHeight(notification)
             
-            self.view.frame.origin.y += getKeyboardHeight(notification)
-           
+            // Since we know that the origin point of the frame is (0,0). The height is always zero at its default position.
+            self.view.frame.origin.y = 0.0
+            
         }
-        
     }
     
     func getKeyboardHeight(notification: NSNotification) -> CGFloat {
